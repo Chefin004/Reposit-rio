6b8db34d72577d28235acbadc3973b790a4a83ae
gabriel.fsilva128 <gabriel.fsilva128@L130587924W10-1>
2022-08-25 14:38:24 -0300

    Adicionar arquivos de projeto.

---
+++ tabuada/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um numero");
+            int n = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("=============");
+            Console.WriteLine("tabuada");
+            Console.WriteLine("=============");
+
+            Console.WriteLine("01 x" + n + " = " + (1 * n));
+            Console.WriteLine("02 x" + n + " = " + (2 * n));
+            Console.WriteLine("03 x" + n + " = " + (3 * n));
+            Console.WriteLine("04 x" + n + " = " + (4 * n));
+            Console.WriteLine("05 x" + n + " = " + (5 * n));
+            Console.WriteLine("06 x" + n + " = " + (6 * n));
+            Console.WriteLine("07 x" + n + " = " + (7 * n));
+            Console.WriteLine("08 x" + n + " = " + (8 * n));
+            Console.WriteLine("09 x" + n + " = " + (9 * n));
+            Console.WriteLine("10 x" + n + " = " + (10 * n));
+            
+
+        }
+    }
+}
